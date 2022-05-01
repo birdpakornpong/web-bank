ee1d12d430fba3babce274af26243b15f9d21cbc
KHUNBIRD <birdpakornpong@gmail.com>
2022-05-02 04:16:07 +0700

    check address

--- src/components/TransferForm.js
+++ src/components/TransferForm.js
@@ -1,14 +1,25 @@
-import React from "react";
-import { useForm } from "react-hook-form";
+import React, { useState } from "react";
+import { useForm, Controller  } from "react-hook-form";
 import { numberFormat } from "../utils/util";
+import { web3 } from '../utils/interact';
 
 export default function DepositWithdrawForm (props) {
+
     const { confirmButton, maxAmount } = props
 
-    const { register, formState: { errors }, handleSubmit } = useForm();
+    const { register, formState: { errors }, handleSubmit } = useForm({
+        mode: "onChange"
+      });
+      
+    const [errorAddress, setErrorAddress] = useState(false)
 
     async function confirm(data) {
-        confirmButton(data.address, data.amount)
+        const checkAddress = await web3.utils.isAddress(data.address);
+        if (checkAddress) {
+            confirmButton(data.address, data.amount)
+        } else {
+            setErrorAddress(true)
+        }     
     }
 
     const onSubmit = data => confirm(data);
@@ -17,6 +28,7 @@ export default function DepositWithdrawForm (props) {
         <form onSubmit={handleSubmit(onSubmit)}>
             <input {...register("address", { required: true })} />
             <span> {errors.address && "Address must request"}</span>
+            <span> {errorAddress && "is not Address"}</span>
 
             <input type="number" {...register("amount", { required: true, min: 0, max: maxAmount})} />
             <span> {errors.amount && `Amount must more 0 and less ${numberFormat(maxAmount)}`}</span>    
