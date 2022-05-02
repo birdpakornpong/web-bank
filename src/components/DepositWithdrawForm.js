2319bbb4556960222f767a2c7911cfa24f108c67
KHUNBIRD <birdpakornpong@gmail.com>
2022-05-02 13:33:09 +0700

    edit color input show error

--- src/components/DepositWithdrawForm.js
+++ src/components/DepositWithdrawForm.js
@@ -20,7 +20,7 @@ export default function DepositWithdrawForm (props) {
         <div className='layout-deposit-form'>
             <form onSubmit={handleSubmit(onSubmit)}>
                 <label className="text-label">Amount</label>
-                <input className="input-custom" type="number" {...register("amount", { required: true, min: 1, max: maxAmount})} placeholder="Enter Amount" />
+                <input className={errors.amount ? "input-custom-error" : "input-custom"} type="number" {...register("amount", { required: true, min: 1, max: maxAmount})} placeholder="Enter Amount" />
                 <span className="error-text"> {errors.amount && `Amount must more 0 and less ${numberFormat(maxAmount)}`}</span>   
                 <div className="layout-button">
                     <input type="submit" className="button-submit"/>
